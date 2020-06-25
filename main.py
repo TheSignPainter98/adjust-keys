6ed3e2beec4cc7fbd9f791376e35f87f55f263c7
Ed Jones <edjones98@gmail.com>
2020-06-25 20:26:27 +0100

    Obtain relation of keys

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 # This was written on literally the hottest day of the year :(
+# This was written the very next day, also on the hottest day of the year D:
 
 
 from args import parse_args
@@ -23,6 +24,7 @@ from layout import Layout
 from log import init_logging, printe, printi
 from sys import argv, exit
 from yaml_io import read_yaml, write_yaml
+from util import nat_join
 
 ##
 # @brief Entry point function, should be treated as the first thing called
@@ -35,9 +37,13 @@ def main(args:[str]) -> int:
     init_logging(pargs.verbose, pargs.quiet)
 
     #  profile = read_yaml(pargs.profile_file)
-    #  glyph_offsets = read_yaml(pargs.glyph_offset_file)
-    layout = Layout(read_yaml(pargs.layout_file))
-    #  glyph_map = read_yaml(pargs.glyph_map_file)
+    glyph_offsets = read_yaml(pargs.glyph_offset_file)
+    #  layout = Layout(read_yaml(pargs.layout_file))
+    glyph_map = read_yaml(pargs.glyph_map_file)
+    #  print(glyph_map[0])
+    glyph_rel = list(map(lambda m: { 'key': m[0], 'glyph': m[1] }, glyph_map.items()))
+
+    write_yaml('-', glyph_rel)
 
     return 0
 
