f239c47c8317527209948e2c1a8c115f37bb10e7
Ed Jones <edjones98@gmail.com>
2020-06-26 16:06:28 +0100

    Added documentation

--- log.py
+++ log.py
@@ -20,6 +20,13 @@ from sys import argv, stderr
 verbose:bool = False
 quiet:bool = False
 
+##
+# @brief Initialise logging, should be called before any logging is to take place
+#
+# @param verbose_in:bool Whether to output verbosely
+# @param quiet_in:bool Whether to output quietly
+#
+# @return Nothing
 def init_logging(verbose_in:bool, quiet_in:bool):
     global quiet, verbose
     if verbose_in and quiet_in:
@@ -27,17 +34,45 @@ def init_logging(verbose_in:bool, quiet_in:bool):
     verbose = verbose_in
     quiet = quiet_in
 
+##
+# @brief Write a message to stderr and exit
+#
+# @param args Message things
+# @param kwargs key-value message things
+#
+# @return Nothing
 def die(*args, **kwargs):
     printe(*args, **kwargs)
     exit(1)
 
+##
+# @brief Output information to stderr
+#
+# @param args Message things
+# @param kwargs More message things
+#
+# @return Nothing
 def printi(*args, **kwargs):
     if verbose:
         print(*args, file=stderr, **kwargs)
 
+##
+# @brief Output an error to stderr
+#
+# @param args Message things
+# @param kwargs More message things
+#
+# @return Nothing
 def printe(*args, **kwargs):
     print(f'{argv[0]}:', *args, file=stderr, **kwargs)
 
+##
+# @brief Output a warning on stderr
+#
+# @param args Message things
+# @param kwargs More message things
+#
+# @return Nothing
 def printw(*args, **kwargs):
     if not quiet:
         print(f'{argv[0]}: WARNING:', *args, **kwargs)
--- util.py
+++ util.py
@@ -18,23 +18,69 @@
 from functools import reduce
 from log import die
 
-def append(a:list, b:list) -> list:
+##
+# @brief Concatenate a pair of lists (pure)
+#
+# @param a:list A list
+# @param b:list Another list
+#
+# @return The concatenation of `a` and `b`
+def concat(a:list, b:list) -> list:
     return a + b
 
+##
+# @brief Compute the intersection of a pair of same-type lists
+#
+# @param a:list A list
+# @param b:list Another list
+#
+# @return The list containing all the items present in both `a` and `b`
 def list_intersection(a:list, b:list) -> list:
     return [x for x in a if x in b]
 
-def nat_join(las:[dict], ka:object, lbs:[dict], kb:object) -> [dict]:
+##
+# @brief The inner join of a pair of lists of dictionaries using a pair of keys.
+# Outputs the list of unions of dictionaries in the input lists which share the same values at the specified keys
+#
+# @param las:[dict] A  list of dictionaries
+# @param ka:object A key present in all of `las` used to join by equality
+# @param lbs:[dict] Another list of dictionaries
+# @param kb:object A key present in all of `lbs` used to join by equality
+#
+# @return A list of united, equal-key dictionaries [union(la, lb) | ls <- las, lb <- lbs, la[ka] == lb[kb]]
+def inner_join(las:[dict], ka:object, lbs:[dict], kb:object) -> [dict]:
     return list(reduce(append, list(map(lambda la: [dict_union(la, lb) for lb in lbs if la[ka] == lb[kb]], las))))
 
+##
+# @brief Output the union of a pair of dictionaries
+#
+# @param a:dict A dictionary
+# @param b:dict Another dictionary
+#
+# @return The union of `a` and `b`, where a key is present in both, the value from `b` is used
 def dict_union(a:dict, b:dict) -> dict:
     return dict(a, **b)
 
+##
+# @brief Replace some key with another while preserving the value
+#
+# @param a:dict A dictionary
+# @param k1:object The old key
+# @param k2:object The new key
+#
+# @return The value of `a` with (k1,v) removed and (k2,v) united for v in ran(a)
 def key_subst(a:dict, k1:object, k2:object) -> dict:
     t = rem(a, k1)
     t[k2] = a[k1]
     return t
 
+##
+# @brief Remove some key from `d` and return the result
+#
+# @param d:dict A dictionary
+# @param k A key to remove
+#
+# @return A copy of `d` with (k,v) removed
 def rem(d:dict, k) -> dict:
     if k not in d:
         die(f'Key {k} not in {d}')
