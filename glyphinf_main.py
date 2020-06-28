1cd8ab6224dd4ae8222ff1642deae85b4f016109
Ed Jones <edjones98@gmail.com>
2020-06-28 16:59:34 +0100

    Added help message

--- glyphinf_main.py
+++ glyphinf_main.py
@@ -11,13 +11,22 @@ from yaml_io import write_yaml
 
 
 def main(args: [str]) -> int:
-    write_yaml('/dev/null', list(map(lambda a: glyph_inf(basename(a)[:-4], a), args[1:])))
-    if len(args) == 1:
-        die('Insufficient arguments, please pass at least one .svg file')
-    elif any(list(filter(lambda a: not a.endswith('.svg'), args[1:]))):
-        die('Only svg file-names are valid, the following have the wrong extension:', ' '.join(list(filter(lambda a: not a.endswith('.svg'), args[1:]))))
+    fargs:[str] = list(filter(lambda a: not a.startswith('-'), args[1:]))
+    oargs:[str] = list(filter(lambda a: a.startswith('-'), args[1:]))
 
-    glyphInf:dict = dict(reduce(lambda a,b: dict_union(a,b), map(lambda a: glyph_inf(basename(a)[:-4], a), args[1:]), {}))
+    if '-h' in oargs or '--help' in oargs:
+        print('Usage: %s /path/to/glyphs/*.svg' % args[0])
+        return 0
+    if any(list(filter(lambda o: o not in ['-h', '--help'], oargs))):
+        printe('Unrecognised arguments: %s' % ' '.join(list(filter(lambda o: o not in ['-h', '--help'], oargs))))
+        return 1
+
+    if len(fargs) == 0:
+        die('Insufficient arguments, please pass at least one .svg file')
+    elif any(list(filter(lambda a: not a.endswith('.svg'), fargs[1:]))):
+        die('Only svg file-names are valid, the following have the wrong extension:', ' '.join(list(filter(lambda a: not a.endswith('.svg'), fargs[1:]))))
+
+    glyphInf:dict = dict(reduce(lambda a,b: dict_union(a,b), map(lambda f: glyph_inf(basename(f)[:-4], f), fargs[1:]), {}))
     write_yaml('-', glyphInf)
     return 0
 
