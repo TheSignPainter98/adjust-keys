cb43990f6d0a67222e6ea36fa4d241e2ee4ac0f9
Ed Jones <edjones98@gmail.com>
2020-06-29 16:20:14 +0100

    276 default unit length... not a fan of the hard-coding

--- args.py
+++ args.py
@@ -34,7 +34,7 @@ def parse_args(args:[str]) -> Namespace:
     ap.add_argument('-g', '--list-glyphs', action='store_true', dest='listGlyphs', help='Output a list of known glyphs read from the input files', default=0)
     ap.add_argument('-k', '--list-keys', action='store_true', dest='listKeys', help='Output a list of known key names read form the input files', default=0)
     ap.add_argument('-@', '--args', action='store', dest='opt_file', help='specify a YAML option file to be take read initial argument values from (default: opts.yml)', default='opts.yml', metavar='file')
-    ap.add_argument('-u', '--unit-length', action='store', type=float, dest='unit_length', help='Specify the length of one unit, that is, the width of a 1u keycap(default: 1.0)', default=1.0, metavar='num')
+    ap.add_argument('-u', '--unit-length', action='store', type=float, dest='unit_length', help='Specify the length of one unit, that is, the width of a 1u keycap (default: 276.0)', default=276.0, metavar='num')
     ap.add_argument('-x', '--delta-x', action='store', type=float, dest='delta_x', help='Horizontal distance between adjacent keycaps without a separating margin', default=0.0, metavar='num')
     ap.add_argument('-y', '--delta-y', action='store', type=float, dest='delta_y', help='Vertical distance between adjacent keycaps without a separating margin', default=0.0, metavar='num')
     ap.add_argument('-X', '--global-x-offset', action='store', type=float, dest='global_x_offset', help='global offset which moves every element to the right (default: 0.0)', default=0.0, metavar='num')
