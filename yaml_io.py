e73b76a91c5047be13f2a70dce1ef08b1db7ef6f
Ed Jones <edjones98@gmail.com>
2020-06-29 22:23:49 +0100

    Added static checking with Cython, applied changes

--- yaml_io.py
+++ yaml_io.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
-from log import printe
+from log import printe, printi
 from os.path import exists
 from sys import stdin
 from yaml import dump, FullLoader, load
