01f7ae47ea87744a839d2d49cd640d79ccec781f
Ed Jones <edjones98@gmail.com>
2020-07-02 15:31:23 +0100

    Unified glyphinf and adjustkeys

--- glyphinf.py
+++ glyphinf.py
@@ -25,45 +25,17 @@ def main(args: [str]) -> int:
     elif any(list(filter(lambda a: not a.endswith('.svg'), fargs))):
         die('Only svg file-names are valid, the following have the wrong extension:', ' '.join(list(filter(lambda a: not a.endswith('.svg'), fargs))))
 
-    glyphInf:dict = dict(reduce(lambda a,b: dict_union(a,b), map(lambda f: glyph_inf(basename(f)[:-4], f), fargs), {}))
+    glyphInf:dict = dict(reduce(lambda a,b: dict_union(a,b), map(lambda f: glyph_inf(f), fargs), {}))
     write_yaml('-', glyphInf)
     return 0
 
 
-def glyph_inf(gname: str, gpath: str) -> dict:
+def glyph_inf(gpath: str) -> dict:
+    gname:str = basename(gpath)[:-4]
     gfile: Document = parse(gpath).documentElement
     svg_width:float = float(gfile.attributes['width'].value)
     svg_height:float = float(gfile.attributes['height'].value)
-    return { gname : { 'glyph-src-width': float(svg_width), 'glyph-src-height': float(svg_height), 'src': gpath } }
-
-
-def necessary_for_glyphs(gname:str, elem: Element) -> bool:
-    return glyph_part(gname, elem) or has_glyph_child(gname, elem)
-
-
-def glyph_part(gname:str, elem: Element) -> bool:
-    return is_glyph(gname, elem) or (elem.parentNode and glyph_part(gname, elem.parentNode))
-
-
-def has_glyph_child(gname:str, elem: Element) -> bool:
-    # This line causes horrendous complexity and dynamic programming would have this sorted just fine, but I take no responsibility for my actions this day.
-    return is_glyph(gname, elem) or (elem.childNodes and any(
-        list(map(lambda c: has_glyph_child(gname, c), elem.childNodes))))
-
-
-def is_glyph(gname:str, elem: Element) -> bool:
-    return elem.attributes and 'id' in elem.attributes and elem.attributes['id'].value == gname
-
-
-def node_list(elem: Element) -> [Element]:
-    def append(l1: list, l2: list) -> list:
-        return l1 + l2
-
-    def fresh(e: list) -> list:
-        return e if e else []
-
-    return [elem] + list(
-            reduce(append, list(map(node_list, fresh(elem.childNodes))), []))
+    return { 'glyph': gname, 'glyph-src-width': float(svg_width), 'glyph-src-height': float(svg_height), 'src': gpath }
 
 
 if __name__ == '__main__':
