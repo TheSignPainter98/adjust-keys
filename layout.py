63ef63e7eb5ebc35a8e7b25416676f723b8933e6
Ed Jones <edjones98@gmail.com>
2020-07-05 20:03:09 +0100

    Use non-unique key names to simplify glyph mapping to handle multiple keys

--- layout.py
+++ layout.py
@@ -20,31 +20,38 @@ from re import match
 from util import dict_union, key_subst, rem, safe_get
 
 
-def parse_layout(layout_row_profiles:[str], layout: [[dict]]) -> [dict]:
-    seenKeys: 'str->int' = {}
+def parse_layout(layout_row_profiles: [str], layout: [[dict]]) -> [dict]:
     if len(layout_row_profiles) < len(layout):
-        printw('Insufficient information about what profile part each row has (e.g. the top row might be r5: got %d but should have at least %d' %(len(layout_row_profiles), len(layout)))
+        printw(
+            'Insufficient information about what profile part each row has (e.g. the top row might be r5: got %d but should have at least %d'
+            % (len(layout_row_profiles), len(layout)))
 
-    def parse_key(key: 'either str dict', nextKey: 'maybe (either str dict)') -> [int,dict]:
+    def parse_key(key: 'either str dict',
+                  nextKey: 'maybe (either str dict)') -> [int, dict]:
         ret: dict
         shift: int = 1
-        def parse_name(txt:str) -> str:
-            parts:[str] = list(reversed(txt.split('\n')))
-            alphaNums:[str] = list(filter(lambda p: match(r'[A-Za-z0-9]+\Z', p), parts))
-            return str(alphaNums[0] if alphaNums != [] else str(max(parts, default=0, key=len)))
+
+        def parse_name(txt: str) -> str:
+            parts: [str] = list(reversed(txt.split('\n')))
+            alphaNums: [str] = list(
+                filter(lambda p: match(r'[A-Za-z0-9]+\Z', p), parts))
+            return str(alphaNums[0] if alphaNums != [] else str(
+                max(parts, default=0, key=len)))
+
         if type(key) == str:
-            ret = { 'key': parse_name(key) }
+            ret = {'key': parse_name(key)}
         elif type(key) == dict:
             if nextKey is not None and type(nextKey) == str:
-                ret = dict_union(key, { 'key': parse_name(nextKey) })
+                ret = dict_union(key, {'key': parse_name(nextKey)})
                 shift = 2
             else:
                 ret = dict(key)
         else:
-            die('Malformed data when reading %s and %s' %(str(key), str(layout)))
+            die('Malformed data when reading %s and %s' %
+                (str(key), str(layout)))
 
         if 'key-type' not in ret:
-            ret_key:str = safe_get(ret, 'key')
+            ret_key: str = safe_get(ret, 'key')
             if safe_get(ret, 'x') == 0.25 \
                 and safe_get(ret, 'a') == 7 \
                 and safe_get(ret, 'w') == 1.25 \
@@ -52,11 +59,12 @@ def parse_layout(layout_row_profiles:[str], layout: [[dict]]) -> [dict]:
                 and safe_get(ret, 'w2') == 1.5 \
                 and safe_get(ret, 'h2') == 1 \
                 and safe_get(ret, 'x2') == -0.25:
-                    ret['key-type'] = 'iso-enter'
+                ret['key-type'] = 'iso-enter'
             elif ret_key == '+' and safe_get(ret, 'h') == 2:
-                    ret['key-type'] = 'num-plus'
-            elif ret_key and ret_key.lower() == 'enter' and safe_get(ret, 'h') == 2:
-                    ret['key-type'] = 'num-enter'
+                ret['key-type'] = 'num-plus'
+            elif ret_key and ret_key.lower() == 'enter' and safe_get(ret,
+                                                                     'h') == 2:
+                ret['key-type'] = 'num-enter'
 
         if 'a' in ret:
             ret = rem(ret, 'a')
@@ -77,34 +85,32 @@ def parse_layout(layout_row_profiles:[str], layout: [[dict]]) -> [dict]:
         if 'key' not in ret:
             printw("Key \"%s\" %s 'key' field, please put one in" %
                    (str(key), 'missing' if key != '' else 'has empty'))
-            ret['key'] = 'SOME_ID'
-        if ret['key'] in seenKeys.keys():
-            seenKeys[ret['key']] += 1
-            ret['key'] = ret['key'] + '-' + str(seenKeys[ret['key']])
-        else:
-            seenKeys[ret['key']] = 1
+            ret['key'] = 'SOME_ID@' + hex(id(key))
 
         return (shift, ret)
 
-    if type(layout) != list and any(list(map(lambda l: type(l) != list, layout))):
+    if type(layout) != list and any(
+            list(map(lambda l: type(l) != list, layout))):
         die('Expected a list of lists in the layout (see the JSON output of KLE)'
             )
 
     parsed_layout: [dict] = []
     row: float = 0.0
     numDeltaY: int = -1
-    lineInd:int = 0
+    lineInd: int = 0
     for line in layout:
         col: float = 0.0
         prevCol: float = 0.0
         numDeltaX: int = 0
-        i:int = 0
+        i: int = 0
         numDeltaY += 1
         while i < len(line):
             # Parse for the next key
-            printi('Handling layout, looking at pair "%s" and "%s"' %(str(line[i]).replace('\n', '\\n'), str(safe_get(line, i+1)).replace('\n', '\\n')))
-            (shift, line[i]) = parse_key(line[i], safe_get(line, i+1))
-            key:dict = line[i]
+            printi('Handling layout, looking at pair "%s" and "%s"' %
+                   (str(line[i]).replace('\n', '\\n'),
+                    str(safe_get(line, i + 1)).replace('\n', '\\n')))
+            (shift, line[i]) = parse_key(line[i], safe_get(line, i + 1))
+            key: dict = line[i]
 
             # Handle shifts and deltas
             if 'shift-y' in key:
@@ -135,7 +141,7 @@ def parse_layout(layout_row_profiles:[str], layout: [[dict]]) -> [dict]:
 
             # Move to the next pair
             i += shift
-        if len(line) >1 and 'shift-y' not in line[-1]:
+        if len(line) > 1 and 'shift-y' not in line[-1]:
             row += 1
         lineInd = min([lineInd + 1, len(layout_row_profiles) - 1])
 
