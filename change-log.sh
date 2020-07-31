34056adf63117c97d76c9e5409757cf0ed6880d4
Ed Jones <edjones98@gmail.com>
2020-07-31 16:44:11 +0100

    Include pull-request merges in the change log

--- change-log.sh
+++ change-log.sh
@@ -2,8 +2,8 @@
 
 echo "# Change Log"
 git log --pretty=format:'%d@%s' \
-	| grep -vEe '^(\(*.*\))*@Merge' \
-	| grep -v '^@Initial commit' \
-	| grep -vEi '\(user devices (not |un)affect?ed' \
-	| uniq \
-	| awk -F')' -f change-log-format.awk
+  | grep -vEe '^(\(*.*\))*@Merge branch' \
+  | grep -v '^@Initial commit' \
+  | grep -vEi '\(user devices (not |un)affect?ed' \
+  | uniq \
+  | awk -F')' -f change-log-format.awk
