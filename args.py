47ce7537bac87af651f9ab4be96f41029153639f
Ed Jones <edjones98@gmail.com>
2020-07-31 16:37:22 +0100

    Added description to help

--- args.py
+++ args.py
@@ -9,6 +9,8 @@ from util import dict_union
 from version import version
 from yaml_io import read_yaml, write_yaml
 
+description:str = 'This is a python script which generates layouts of keycaps and glyphs for (automatic) import into Blender! Gone will be the days of manually placing caps into the correct locations and spending hours fixing alignment problems of glyphs on individual keys - simply specify the layout you want using the JSON output of KLE to have the computer guide the caps into the mathematically-correct locations. This script can be used to create a single source of truth for glyph alignment on caps, so later changes and fixes can be more easily propagated.'
+
 
 ##
 # @brief Parse commandline arguments
@@ -18,7 +20,7 @@ from yaml_io import read_yaml, write_yaml
 #
 # @return A namespace of options
 def parse_args(args:[str]) -> Namespace:
-    ap:ArgumentParser = ArgumentParser()
+    ap:ArgumentParser = ArgumentParser(description=description)
 
     # Default values
     dargs:dict = {
