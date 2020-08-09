ab64aa50a2150885fee14276ba68ba29d2b8d2bc
Ed Jones <edjones98@gmail.com>
2020-08-09 19:41:07 +0100

    Strip away svg units, if present

--- glyphinf.py
+++ glyphinf.py
@@ -2,14 +2,18 @@
 # Copyright (C) Edward Jones
 
 from os.path import basename
+from re import search
 from xml.dom.minidom import Document, parse
 
+numRegex:str = r'([0-9]*\.[0-9]+|[0-9]+)'
 
 def glyph_inf(gpath: str) -> dict:
     gname: str = glyph_name(gpath)
     gfile: Document = parse(gpath).documentElement
-    svg_width: float = float(gfile.attributes['width'].value)
-    svg_height: float = float(gfile.attributes['height'].value)
+    rawWidth:str = search(numRegex, gfile.attributes['width'].value).group(0)
+    rawHeight:str = search(numRegex, gfile.attributes['height'].value).group(0)
+    svg_width: float = float(rawWidth)
+    svg_height: float = float(rawHeight)
     return {
         'glyph': gname,
         'glyph-src-width': float(svg_width),
