1666da89e2cc95898c8cc657d7dfb0b689bd907a
Ed Jones <edjones98@gmail.com>
2020-09-20 16:29:10 +0100

    Fixed effect of colour resolver to work with cap and glyph colouring

--- adjustkeys/colour_resolver.py
+++ adjustkeys/colour_resolver.py
@@ -4,11 +4,11 @@ from re import IGNORECASE, match
 
 def colourise_layout(layout:[dict], colour_map:[dict]) -> [dict]:
     for key in layout:
-        apply_colouring(key, colour_map, 'cap-colour', 'cap-colour-raw')
-        apply_colouring(key, colour_map, 'glyph-style', 'glyph-colour-raw')
+        apply_colouring(key, colour_map, 'cap-colour', 'cap-colour-raw', 'name')
+        apply_colouring(key, colour_map, 'glyph-style', 'glyph-colour-raw', 'glyph-style')
     return layout
 
-def apply_colouring(key:dict, colour_map:[dict], target_key:str, raw_key:str):
+def apply_colouring(key:dict, colour_map:[dict], target_key:str, raw_key:str, extraction_key:str):
     if raw_key in key:
         key[target_key] = key[raw_key]
     else:
@@ -16,6 +16,6 @@ def apply_colouring(key:dict, colour_map:[dict], target_key:str, raw_key:str):
             key_name = key['key']
             for mapping in colour_map:
                 if target_key in mapping and any(map(lambda r: match('^' + r + '$', key_name, IGNORECASE) is not None, mapping['keys'])):
-                    key[target_key] = mapping['name']
+                    key[target_key] = mapping[extraction_key]
                     return
         key[target_key] = None
