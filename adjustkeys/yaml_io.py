74f2f4340c635711bf162a8be710b259c11fb838
Ed Jones <edjones98@gmail.com>
2020-10-07 14:19:01 +0100

    Allow tabs in yaml/json inputs

--- adjustkeys/yaml_io.py
+++ adjustkeys/yaml_io.py
@@ -2,6 +2,7 @@
 
 from .log import die, printi
 from os.path import exists
+from re import Match, search
 from sys import stdin
 from yaml import dump, safe_load
 
@@ -13,17 +14,31 @@ from yaml import dump, safe_load
 #
 # @return The data present in file fname
 def read_yaml(fname: str) -> dict:
+    yaml_lines:[str]
     if fname == '-':
         printi(
             'Reading %s from stdin, please either type something or have redirected a file in here'
             % fname)
-        return safe_load(stdin)
+        yaml_lines = stdin.read().split('\n') # stdin.readlines() seems to do something silly with the symbols
     else:
         if exists(fname):
             with open(fname, 'r', encoding='utf-8') as f:
-                return safe_load(f)
+                yaml_lines = f.read().split('\n') # f.readlines() seems to do something silly with the symbols
+
         else:
             die('Couldn\'t find or read file "%s"' % fname)
+    sanitised_yaml_string:str = '\n'.join(list(map(sanitise_yaml_line, yaml_lines)))
+    return safe_load(sanitised_yaml_string)
+
+
+def sanitise_yaml_line(line:str) -> str:
+    m:Match = search(r'^\t+', line)
+    if m is not None:
+        span:tuple = m.span()
+        num_tabs:int = span[1] - span[0]
+        return '  ' * num_tabs + line[num_tabs:]
+    else:
+        return line
 
 
 ##
