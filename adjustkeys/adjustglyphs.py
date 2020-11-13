c20eb160b4a718a7f334adf217ae2844525259e1
Ed Jones <edjones98@gmail.com>
2020-11-13 17:53:16 +0000

    Fix paranting from different collection issue

--- adjustkeys/adjustglyphs.py
+++ adjustkeys/adjustglyphs.py
@@ -79,10 +79,8 @@ def adjust_glyphs(layout:[dict], profile_data:dict, collection:Collection, glyph
     collectionName:str = get_only(list_diff(collectionsPostImport, collectionsPreImport), 'No collections added when importing glyphs', 'Multiple collections added whilst importing glyphs, got %d: %s')
 
     printi('Adding svg into the right collection')
-    glyph_collection = make_collection('glyphs', parent_collection=collection)
-    #  collection.children.link(glyph_collection)
     for svgObjectName in svgObjectNames:
-        glyph_collection.objects.link(data.objects[svgObjectName])
+        collection.objects.link(data.objects[svgObjectName])
     data.collections.remove(data.collections[collectionName])
 
     # Apprpriately scale the objects
